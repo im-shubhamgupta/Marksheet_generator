e7785781203799503419e973c33ef6aab9c060b4
Shubham kumar Gupta <shubhamgupta309@gmail.com>
2024-12-24 00:05:47 +0530

    add all files

---
+++ src/constant.js
@@ -0,0 +1,14 @@
+
+
+// export default {
+//     API_URL: "https://api.example.com",
+//     TIMEOUT: 5000,
+// };
+
+function getConstants() {
+    return {
+        SCHOOL: "U2lzaHUgVmlrYXNoIFZpZHlhIE1hbmRpcg==",
+        REG: "123456789",
+        TIMEOUT: 5000,
+    };
+}
